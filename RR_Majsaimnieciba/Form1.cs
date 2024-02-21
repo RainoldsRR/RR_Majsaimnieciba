75620c7f413608afa52666d5473b8a1c964e04e6
Rainolds Rusiņš <rr2519@gymn.local>
2024-02-21 10:48:30 +0200

    Beidzamais

--- RR_Majsaimnieciba/Form1.cs
+++ RR_Majsaimnieciba/Form1.cs
@@ -26,17 +26,19 @@ namespace RR_Majsaimnieciba
         {
             double ienakumi = Convert.ToDouble(txtbox_ienakumi1.Text);
             double ienakumi1 = Convert.ToDouble(txtbox_ienakumi2.Text);
-            double izdevumi = Convert.ToDouble(txtbox_izdevumi1);
-            double izdevumi1 = Convert.ToDouble(txtbox_izdevumi2);
-            double izdevumi2 = Convert.ToDouble(txtbox_izdevumi3);
-            double izdevumi3 = Convert.ToDouble(txtbox_izdevumi4);
+            double izdevumi = Convert.ToDouble(txtbox_izdevumi1.Text);
+            double izdevumi1 = Convert.ToDouble(txtbox_izdevumi2.Text);
+            double izdevumi2 = Convert.ToDouble(txtbox_izdevumi3.Text);
+            double izdevumi3 = Convert.ToDouble(txtbox_izdevumi4.Text);
 
             majsm_budzets(ienakumi, ienakumi1, izdevumi, izdevumi1, izdevumi2, izdevumi3);
         }
 
         public void majsm_budzets(double ienakumi_, double ienakumi_1, double izdevumi_, double izdevumi_1, double izdevumi_2, double izdevumi_3)
         {
+            double budzets = (ienakumi_ + ienakumi_1) - (izdevumi_ + izdevumi_1 + izdevumi_2 + izdevumi_3);
 
+            txtbox_budzets.Text = budzets.ToString();
         }
     }
 }
